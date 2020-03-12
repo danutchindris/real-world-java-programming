bb6a7a6a3e69c05510d7fb192c39915005b4382d
Danut Chindris <danutchindris@gmail.com>
2020-03-12 21:53:55 +0200

    solve problem using Streams

--- teo/src/main/java/com/skytravelr/CharCounter.java
+++ teo/src/main/java/com/skytravelr/CharCounter.java
@@ -1,5 +1,7 @@
 package com.skytravelr;
 
+import java.util.stream.Collectors;
+
 /**
  * @author Teo
  * @since 12.03.2020
@@ -7,10 +9,20 @@ package com.skytravelr;
 public class CharCounter {
 
     public static void main(String[] args) {
-        String someString = "elephant";
-        for (char c = 'a'; c <= 'z'; c++){
+        String input = "elephant";
+        /*for (char c = 'a'; c <= 'z'; c++){
             char finalC = c;
             System.out.println(someString.chars().filter(ch -> ch == finalC).count());
+        }*/
+
+        String distinct = input.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .distinct()
+                .collect(Collectors.joining(""));
+
+        for (int i = 0; i < distinct.length(); i++) {
+            char litera = distinct.charAt(i);
+            System.out.print(litera + "" + input.chars().filter(ch -> ch == litera).count());
         }
     }
 }
